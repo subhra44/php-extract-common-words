03fae6c77ee0f52b4e20af1a9d5a56cab13da51f
Subhra <subhra44@gmail.com>
2019-01-13 11:40:16 +0530

    initial commit

---
+++ db.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for macos10.13 (x86_64)
+--
+-- Host: 127.0.0.1    Database: top_words
+-- ------------------------------------------------------
+-- Server version	8.0.13
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `word_count`
+--
+
+DROP TABLE IF EXISTS `word_count`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `word_count` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `word` varchar(255) NOT NULL,
+  `word_count` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word_UNIQUE` (`word`)
+) ENGINE=InnoDB AUTO_INCREMENT=56264 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-13 11:07:07
---
+++ extract-common-words.php
@@ -0,0 +1,103 @@
+<?php
+set_time_limit(0);
+error_reporting(E_ALL);
+
+// Load environment variables
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+// Create a new MySQL database connection
+if (!$con = new mysqli(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME'))) {
+    die('An error occurred while connecting to the MySQL server!' . $con->connect_error);
+}
+
+function crawl_page($url)
+{
+    echo "Crawling " . $url."\n";
+    
+    global $con;
+    $hrefs = [];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+	$response = curl_exec($ch);
+    $curl_info = curl_getinfo( $ch );
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $html = substr($response, $header_size);
+    curl_close($ch);
+
+    if( substr( $curl_info['content_type'], 0, 10 ) !== "text/html;" )
+    {
+        return;
+    }
+	
+	$dom = new DOMDocument();
+    @$dom->loadHTML($html);
+
+    // Remove script tags
+    while (($r = $dom->getElementsByTagName("script")) && $r->length) {
+        $r->item(0)->parentNode->removeChild($r->item(0));
+    }
+    $html = $dom->saveHTML();
+    
+    $plainText = $dom->textContent;
+    $plainText = clean( $plainText );
+    $words = explode( ' ', $plainText );
+    $words = array_filter($words, function($value) {
+        if( $value != '' )
+        {
+            return trim( $value, '-' );
+        }
+    });
+    $words = array_count_values( $words );
+    //print_r( $words );
+
+    foreach($words as $k => $v)
+    {
+        $data[] = "('" . $con->real_escape_string($k) . "',". $v . ")";
+    }
+	
+	if(!empty($data))
+	{
+		$sql = "INSERT INTO word_count (word, word_count) VALUES " . implode(',', $data) . " ON DUPLICATE KEY UPDATE word_count = word_count + VALUES(word_count)";
+        if ($con->query($sql) === FALSE)
+        {
+            echo "Error: " . $sql . "\n" . $con->error;
+        }
+    }
+}
+
+// Function to clean a string
+function clean( $string )
+{
+    $string = str_replace('&nbsp;', ' ', $string); // Replace &nbsp; with space
+    $string = preg_replace('/[^A-Za-z\-\s\']/', '', $string); // Removes special chars.
+    $string = preg_replace('~[\s]+~', ' ', $string); // Remove extra spaces
+    $string = trim( $string );
+
+    return strtolower( $string );
+}
+
+// Crawl the main page
+crawl_page( getenv('URL') );
+
+// Get the top commmon words
+$sql = "SELECT word, word_count FROM word_count ORDER BY word_count DESC LIMIT " . getenv('LIMIT_TOP_COMMON_WORDS');
+if ( $result = $con->query($sql) )
+{
+    echo "Word | Count\n";
+    while( $obj = $result->fetch_object() )
+    { 
+        echo $obj->word . " | " . $obj->word_count . "\n";
+    }
+}
+else
+{
+    echo "Error: " . $sql . "\n" . $con->error;
+}
+
+// Close the mysql connection
+$con->close();
